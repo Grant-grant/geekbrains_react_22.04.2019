888eada787d09f91852d182f957b8b598a09affa
Grant-grant <33813423+Grant-grant@users.noreply.github.com>
2019-05-24 15:34:02 +0300

    профиль
    профиль

--- static_src/components/Profile.jsx
+++ static_src/components/Profile.jsx
@@ -4,7 +4,15 @@ export default class Profile extends React.Component {
 
     render() {
         return (
-            <h1>Profile</h1>
+            <div>
+			<h1>Мой профиль</h1>
+			<b>Фамилия</b><p> Gamz</p>
+			<b>Имя</b> <p>Grant</p>
+			<b>e-mail</b> <p>gamz@rumbler.ru</p>
+			<b>Телефон</b> <p>8(999)999-99-99</p>
+			<b>О себе</b> <p>Average static man</p>					
+			<Link className="side" to={ '../' }> Назад</Link>			
+			</div>
         )
     }
 }
